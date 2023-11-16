8aa913b37390e94ea6244c9cf4fed2a1c6af93ae
unknown <tinotendachibi33@gamil.com>
2023-11-16 12:16:45 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-/** 
- * 
- * 
- * @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-=======
->>>>>>> c74db902ea3d5ce486ba4b31b8c9ff02ebae1990
 module.exports = {
     trailingSlash: true,
     }
